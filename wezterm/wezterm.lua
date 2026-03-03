5c71f605b04a422861f877d93db19584193b40c5
Tanmay Shishodia <tanmay.shishodia@mistral.ai>
2026-03-02 21:33:27 -0800

    Always show tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -36,7 +36,7 @@ end)
 -- Tab Bar (minimal)
 -- ===========================================
 config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = false
 config.show_new_tab_button_in_tab_bar = false
 
